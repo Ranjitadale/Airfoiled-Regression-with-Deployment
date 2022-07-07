97f5bd981f91f33a0bd562f2271b1734c9883e17
Ranjitadale <99066853+Ranjitadale@users.noreply.github.com>
2022-07-07 11:10:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import pickle
+import sklearn
+from flask import Flask,request,app,jsonify
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+
+    data=request.json['data']
+    print(data)
+    new_data=[list(data.values())]
+    output=model.predict(new_data)[0]
+    return jsonify(output)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
